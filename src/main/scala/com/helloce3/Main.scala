f551c26960a3df78613b40f8b80d92f173a16a93
Ethan Zhang <ethan6077@gmail.com>
2024-05-16 09:13:44 +1000

    foreverM

--- src/main/scala/com/helloce3/Main.scala
+++ src/main/scala/com/helloce3/Main.scala
@@ -4,7 +4,6 @@ import cats.effect.IO
 import cats.effect.IOApp
 
 object Main extends IOApp.Simple:
-
-  // This is your new "main"!
   def run: IO[Unit] =
-    HelloWorld.say().flatMap(IO.println)
+    StupidFizzBuzz.run
+    HelloWorld.say().flatMap(IO.println).foreverM
