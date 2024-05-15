b73309cc7da22ec0af2b13811ac1012b99345d5a
Ethan Zhang <ethan6077@gmail.com>
2024-05-16 09:24:18 +1000

    foreverM with sleep

--- src/main/scala/com/helloce3/Main.scala
+++ src/main/scala/com/helloce3/Main.scala
@@ -1,9 +1,16 @@
 package com.helloce3
 
-import cats.effect.IO
-import cats.effect.IOApp
+import cats.effect.{IO, IOApp, Temporal}
+
+import scala.concurrent.duration.*
 
 object Main extends IOApp.Simple:
   def run: IO[Unit] =
-    StupidFizzBuzz.run
-    HelloWorld.say().flatMap(IO.println).foreverM
+//    StupidFizzBuzz.run
+    val loop: IO[Unit] =
+      for
+        _ <- HelloWorld.say().flatMap(IO.println)
+        _ <- IO.defer(Temporal[IO].sleep(1.seconds))
+      yield ()
+
+    loop.foreverM
